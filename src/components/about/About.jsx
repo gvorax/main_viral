7239f8b4b4a9d085760d4d9a058fa2f58b01b879
gvorax <otabek83019@gmail.com>
2023-12-14 09:53:36 +0500

    added

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./about.scss";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectCoverflow, Pagination } from "swiper/modules";
 
 import "swiper/css";
-import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay} from "swiper/modules";
+
 import { useTranslation } from "react-i18next";
+import Images from "../../assets/bgInter.jpg"
 
 const About = () => {
   const [more, setMore] = React.useState(false);
@@ -31,15 +33,19 @@ const About = () => {
         </svg>
       </span>
       <p data-aos="fade-up">
-       {t("aboutText")}
-        {more && (
-          <span>
-            {t("aboutText2")}
-          </span>
-        )}
+        {t("aboutText")}
+        {more && <span>{t("aboutText2")}</span>}
       </p>
       <div className="subLink" data-aos="fade-up">
-        <h3 className="link" onClick={()=>{setMore(!more)}} > {!more ? t("more"):t("less")} </h3>
+        <h3
+          className="link"
+          onClick={() => {
+            setMore(!more);
+          }}
+        >
+          {" "}
+          {!more ? t("more") : t("less")}{" "}
+        </h3>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="22"
@@ -60,50 +66,26 @@ const About = () => {
 
       <Swiper
         data-aos="fade-up"
-        effect={"coverflow"}
-        grabCursor={true}
+        spaceBetween={30}
         centeredSlides={true}
-        slidesPerView={"auto"}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
         }}
-        autoplay={{ delay: 2500 }}
-        loop={true}
-        // pagination={true}
-        modules={[EffectCoverflow, Pagination]}
+        modules={[Autoplay]}
         className="mySwiper"
       >
         <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+          <img src={Images} alt="" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
-        </SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
       </Swiper>
     </div>
   );
